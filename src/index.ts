bf26ff03cb642a349f8d41d178a15de8a5842d51
Rabea <rabeea@city-furniture.com>
2021-12-21 09:57:58 -0500

    check for NR key before starting

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,24 @@ interface Params {
     interval: number;
 }
 
-const endpoint: Params["endpoint"] = process.env.PING_HOST as string
-const intervalString: string = process.env.PING_INTERVAL as string
-const interval: number = Number.parseInt(intervalString, 10)
-
-async function main(endpoint: Params["endpoint"]): Promise<void> {
-    try {
-        let res = await ping.promise.probe(endpoint)
-        const nrEvent = res as any
-        nrEvent.times = JSON.stringify(res.times)
-        NR.recordCustomEvent('host_ping', nrEvent)
-    } catch (err) {
-        console.log(err)
+if ('NEWRELIC_LICENSE_KEY' in process.env) {
+    const endpoint: Params["endpoint"] = process.env.PING_HOST as string
+    const intervalString: string = process.env.PING_INTERVAL as string
+    const interval: number = Number.parseInt(intervalString, 10)
+    
+    async function main(endpoint: Params["endpoint"]): Promise<void> {
+        try {
+            let res = await ping.promise.probe(endpoint)
+            const nrEvent = res as any
+            nrEvent.times = JSON.stringify(res.times)
+            NR.recordCustomEvent('host_ping', nrEvent)
+        } catch (err) {
+            console.log(err)
+        }
     }
+    
+    setInterval(() => { main(endpoint) }, interval)
+} else {
+    console.log('No NEWRELIC_LICENSE_KEY found.')
 }
-
-setInterval(() => { main(endpoint) }, interval)
 
